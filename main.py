ae991f8af362975b1d2343b1a981a3cca450818e
Sivanesan007 <144417793+Sivanesan007@users.noreply.github.com>
2023-09-13 13:02:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+year = int(input("Enter a year: "))
+
+# divided by 100 means century year (ending with 00)
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+else:
+    print("{0} is not a leap year".format(year))
